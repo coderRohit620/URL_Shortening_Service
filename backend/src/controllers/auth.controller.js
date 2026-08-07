69990c8f77ece8c808dfcf74a2f178fa61587405
coderRohit620 <rohitravi62023@gmail.com>
2026-08-08 00:22:11 +0530

    enhance authentication flow with token generation and user registration/login updates

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,32 +1,117 @@
 import { loginUser, registerUser } from "../services/auth.service"
+import { ApiResponse } from "../utils/ApiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
 import genrateToken from "../utils/genrateToken";
+import { User } from "../models/user.model.js";
 
 
-const register = async(req, res, next) =>{
-    try {
-        const user = await registerUser(req.body);
-        const token = genrateToken(user._id);
+const generateAccessAndRefereshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
 
-        res.status(201).json({
-            success: true,
-            token,
-            user,
-        });
-    } catch (error) {
-        next(error);
-    }
-}
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
 
-const login = async(req,res,next) =>{
-    try {
-        const user = await loginUser(req.body);
-        const token = genrateToken(user._id);
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generation referesh and access token"
+    );
+  }
+};
 
-        res.status(200).json({
-            success:true,
-            token,user,
-        })
-    } catch (error) {
-        next(error)
-    }
+const register = asyncHandler(async (req, res) => {
+    const user = await registerUser(req.body);
+
+     const { accessToken, refreshToken } =
+        await generateAccessAndRefreshToken(user._id);
+
+    const loggedInUser = await User.findById(user._id)
+        .select("-password -refreshToken");
+
+    return res
+        .status(201)
+        .cookie("accessToken", accessToken, cookieOptions)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .json(
+            new ApiResponse(
+                201,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                },
+                "User registered successfully"
+            )
+        );
+})
+
+const login = asyncHandler(async (req,res) => {
+    const user = await loginUser(req.body);
+    
+    const { accessToken, refreshToken } =
+        await generateAccessAndRefreshToken(user._id);
+
+    const loggedInUser = await User.findById(user._id)
+        .select("-password -refreshToken");
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, cookieOptions)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                },
+                "Login successful"
+            )
+        );
+})
+
+const logout = asyncHandler(async (req,res)=>{
+     await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Logout successful"
+            )
+        );
+});
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            req.user,
+            "Current user fetched successfully"
+        )
+    );
+});
+
+export {
+    login, 
+    register,
+    logout,
+    getCurrentUser,
 }
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -25,6 +25,9 @@ const userSchema = new Schema(
       minlength: 6,
       select: false,
     },
+    refreshToken:{
+            type:String
+        }
   },
   {
     timestamps: true,
@@ -40,6 +43,34 @@ userSchema.pre("save", async function(){
 // Compare password
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
+}
+
+// Generate JWT token
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this._email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRATION
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRATION
+        }
+    )
 }
 
 export default mongoose.model("User", userSchema);
